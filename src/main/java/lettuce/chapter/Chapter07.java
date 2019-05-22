cc50a66e6bc804cec46c0167eba189e9c5b90d88
Yaoxunyu <windc96@gmail.com>
2019-05-22 16:02:33 +0800

    c 07-7.1

--- src/main/java/lettuce/chapter/Chapter07.java
+++ src/main/java/lettuce/chapter/Chapter07.java
@@ -1,17 +1,22 @@
 package lettuce.chapter;
 
 import com.google.common.collect.Sets;
+import io.lettuce.core.SortArgs;
 import io.lettuce.core.api.reactive.RedisReactiveCommands;
+import lettuce.key.Key07;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static lettuce.key.Key07.*;
+import static lettuce.key.Key07.S_IDX;
 
 /**
  * @author YaoXunYu
@@ -33,12 +38,16 @@ public class Chapter07 extends BaseChapter {
         "wants", "was", "we", "were", "what", "when", "where", "which", "while",
         "who", "whom", "why", "will", "with", "would", "yet", "you", "your");
 
-    private static final Pattern WORDS_RE = Pattern.compile("[a-z']{2,}");
+    private final Pattern WORDS_RE = Pattern.compile("[a-z']{2,}");
+    private final Pattern QUERY_RE = Pattern.compile("[+-]?[a-z']{2,}");
 
     public Chapter07(RedisReactiveCommands<String, String> comm) {
         super(comm);
     }
 
+    /**
+     * TODO strip("'")
+     */
     public Stream<String> tokenize(String content) {
         Iterator<String> iterator = new Iterator<String>() {
             private Matcher matcher = WORDS_RE.matcher(content.toLowerCase());
@@ -50,13 +59,12 @@ public class Chapter07 extends BaseChapter {
 
             @Override
             public String next() {
-                return matcher.group().trim();
+                return matcher.group();
             }
         };
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator,
             Spliterator.ORDERED | Spliterator.NONNULL | Spliterator.IMMUTABLE),
             false)
-            .filter(s -> s.length() > 2)
             .filter(s -> !STOP_WORDS.contains(s));
     }
 
@@ -65,10 +73,145 @@ public class Chapter07 extends BaseChapter {
             .flatMap(word -> comm.sadd(S_IDX(word), docId.toString()));
     }
 
-    public Mono intersect(String[] keys, long timeout) {
-        String id = UUID.randomUUID().toString();
-        return comm.sinterstore(id, keys)
+    public Mono<String> intersect(String[] keys) {
+        return collectAndExpire(id -> comm.sinterstore(id, keys));
+    }
+
+    public Mono<String> intersect(String[] keys, long timeout) {
+        return collectAndExpire(id -> comm.sinterstore(id, keys), timeout);
+    }
+
+    public Mono<String> union(String[] keys) {
+        return collectAndExpire(id -> comm.sunionstore(id, keys));
+    }
+
+    public Mono<String> union(String[] keys, long timeout) {
+        return collectAndExpire(id -> comm.sunionstore(id, keys), timeout);
+
+    }
+
+    public Mono<String> difference(String[] keys) {
+        return collectAndExpire(id -> comm.sdiffstore(id, keys));
+    }
+
+    public Mono<String> difference(String[] keys, long timeout) {
+        return collectAndExpire(id -> comm.sdiffstore(id, keys), timeout);
+    }
+
+    private Mono<String> collectAndExpire(Function<String, Mono<Long>> collectOps) {
+        String id = S_IDX(UUID.randomUUID().toString());
+        return collectOps.apply(id)
+            .then(comm.expire(id, 30))
+            .thenReturn(id);
+    }
+
+    private Mono<String> collectAndExpire(Function<String, Mono<Long>> collectOps, long timeout) {
+        String id = S_IDX(UUID.randomUUID().toString());
+        return collectOps.apply(id)
             .then(comm.expire(id, timeout))
             .thenReturn(id);
     }
+
+    /**
+     * TODO strip("'")
+     *
+     * @return t1: add, t2 unwanted
+     */
+    private Tuple2<List<Set<String>>, Set<String>> parse(String query) {
+        List<Set<String>> all = new ArrayList<>();
+        Set<String> unwanted = new HashSet<>();
+        Set<String> current = new HashSet<>();
+        Matcher matcher = QUERY_RE.matcher(query.toLowerCase());
+        while (matcher.find()) {
+            String word = matcher.group();
+            String prefix = word.substring(0, 1);
+            boolean add = "+".equals(prefix);
+            boolean subtract = "-".equals(prefix);
+            if (add || subtract) {
+                word = word.substring(1);
+            }
+            if (word.length() < 2 || STOP_WORDS.contains(word)) {
+                continue;
+            }
+            if (subtract) {
+                unwanted.add(word);
+                continue;
+            }
+            if (!current.isEmpty() && !add) {
+                all.add(current);
+                current = new HashSet<>();
+            }
+            current.add(word);
+
+        }
+        if (!current.isEmpty()) {
+            all.add(current);
+        }
+        return Tuples.of(all, unwanted);
+    }
+
+    public Mono<String> parseAndSearch(String query, long timeout) {
+        return Mono.just(parse(query))
+            .filter(tuple -> !tuple.getT1().isEmpty())
+            .flatMap(tuple -> {
+                Mono<String> mono = Flux.fromIterable(tuple.getT1())
+                    .map(set -> set.stream()
+                        .map(Key07::S_IDX)
+                        .toArray(String[]::new))
+                    .flatMap(arr -> union(arr, timeout))
+                    .collectList()
+                    .flatMap(list -> intersect(list.toArray(new String[0]), timeout));
+                if (tuple.getT2().isEmpty()) {
+                    return mono;
+                } else {
+                    return mono.flatMap(id -> difference(
+                        Stream.concat(Stream.of(id),
+                            tuple.getT2()
+                                .stream()
+                                .map(Key07::S_IDX))
+                            .toArray(String[]::new),
+                        timeout));
+                }
+            });
+    }
+
+    public Flux<String> searchAndSort(String query, SortArgs sortArgs, long timeout) {
+        return parseAndSearch(query, timeout)
+            .flatMapMany(id -> comm.sort(id, sortArgs));
+    }
+
+    public Mono<SortResult> searchAndSort(String query, SortArgs sortArgs, long timeout, String idx) {
+        return comm.expire(idx, timeout)
+            .filter(b -> b)
+            .map(b -> idx)
+            .switchIfEmpty(parseAndSearch(query, timeout))
+            .flatMap(id -> Mono.zip(comm.scard(id),
+                comm.sort(id, sortArgs).collectList())
+                .map(tuple -> {
+                    SortResult r = new SortResult();
+                    r.id = id;
+                    r.size = tuple.getT1();
+                    r.values = tuple.getT2();
+                    return r;
+                }));
+    }
+
+    private static class SortResult {
+        String id;
+        Long size;
+        List<String> values;
+
+        public String getId() {
+            return id;
+        }
+
+        public Long getSize() {
+            return size;
+        }
+
+        public List<String> getValues() {
+            return values;
+        }
+    }
+
 }
